55dde87f191f7fce8615e4d2d994e96ff39d1033
Pepe <pgarciag93@gmail.com>
2017-03-08 18:02:49 +0100

    Add Bea moment.

--- quotes.js
+++ quotes.js
@@ -5,28 +5,28 @@ var quotes =
             "Voy al cuarto de baño",
             "Pues yo me quedo con el quinto de baño"
         ],
-        author: "Miriam Sanchez"
+        author: "Miriam Sánchez"
     },
     {
         quote: [
             "Tengo que ir a sport pump y después a zumba y hay media hora entre medio que no tengo na que hacé",
             "Pues vete al gimnasio en vez de a por pan"
         ],
-        author: "Beatriz Gonzalez"
+        author: "Beatriz González"
     },
     {
         quote: [
             "Tú no tienes pacientes que atender?",
             "Pero están a gusto porque son pacientes"
         ],
-        author: "Miriam Sanchez"
+        author: "Miriam Sánchez"
     },
     {
         quote: [
             "Te has fijao en el detalle del chalequito atao a la cintura?",
             " Si es un chaleco, como se lo ata si no tiene mangas?"
         ],
-        author: "Miriam Sanchez"
+        author: "Miriam Sánchez"
     },
     {
         quote: [
@@ -35,5 +35,13 @@ var quotes =
             "Pues si estalla no lo metas"
         ],
         author: "Miriam Sanchez"
-    }
+    },
+    {
+        quote: [
+            "Que es curl? El primo de JSON?",
+            "Eh?",
+            "Sí, Curl cobain y JSON derulo",
+        ],
+        author: "Beatriz González."
+    },
 ];
