ecaef2b2879b63cc9c37380a8b9061a9ba6a9ce5
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2018-02-21 15:52:51 +0100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -2,118 +2,16 @@ var quotes =
 [
     {
         quote: [
-            "Voy al cuarto de baño",
-            "Pues yo me quedo con el quinto de baño"
+            "Venga va, os subo empanada",
+            "Con que nos subas la app nos vale"
         ],
-        author: "Miriam Sánchez"
+        author: "Pepón García"
     },
     {
         quote: [
-            "Tengo que ir a sport pump y después a zumba y hay media hora entre medio que no tengo na que hacé",
-            "Pues vete al gimnasio en vez de a por pan"
+            "Para este proyecto no, que voy con retraso",
+            "Y encima el proyecto va mal de tiempo"
         ],
-        author: "Beatriz González"
-    },
-    {
-        quote: [
-            "Tú no tienes pacientes que atender?",
-            "Pero están a gusto porque son pacientes"
-        ],
-        author: "Miriam Sánchez"
-    },
-    {
-        quote: [
-            "Te has fijao en el detalle del chalequito atao a la cintura?",
-            " Si es un chaleco, como se lo ata si no tiene mangas?"
-        ],
-        author: "Miriam Sánchez"
-    },
-    {
-        quote: [
-            "Estamos desarrollando una func....",
-            "Eso está ya",
-            "Pues si estalla no lo metas"
-        ],
-        author: "Miriam Sanchez"
-    },
-    {
-        quote: [
-            "Que es curl? El primo de JSON?",
-            "Eh?",
-            "Sí, Curl cobain y JSON derulo",
-        ],
-        author: "Beatriz González."
-    },
-    {
-        quote: [
-            "Me bootstrap mucho el solecito",
-        ],
-        author: "Miriam Sánchez."
-    },
-    {
-        quote: [
-            "Una amiga de pablo se ha caido y se ha roto la nariz bailando",
-            "bachata?",
-            "Ahora sí"
-        ],
-        author: "Miriam Sánchez."
-    },
-    {
-        quote: [
-            "Sabes quién es el product owner de ATAS?",
-            "Christian Grey"
-        ],
-        author: "Beatriz Gonzalez."
-    },
-    
-    {
-        quote: [
-            "Pues Edu Serrano lleva el proyecto de...",
-            "Edu Serrano y Edu York"
-        ],
-        author: "Beatriz Gonzalez."
-    },
-    {
-        quote: [
-            "Me ha arañado el gato que parece lobezno",
-            "Zi lo veo"
-        ],
-        author: "Miriam Sanchez."
-    },
-    {
-        quote: [
-            "Cómo se ponen los alt en Árabe?",
-            "Pues altrevés"
-        ],
-        author: "Cristina Primera del Sánchez Pizjuan."
-    },
-    {
-        quote: [
-            "¿Cómo se hace el grid?",
-            "Pues un tono así entre el blanco y el negro"
-        ],
-        author: "Joselito huevo frito."
-    },
-    {
-        quote: [
-            "Deberíamos hacer cosas con los nombres de los proyectos",
-            "Que le vamos IAC si esto es lo OCAI"
-        ],
-        author: "Miriam Sanchez."
-    },
-    {
-        quote: [
-            "¿Hasta cuando son los cursos?",
-            "Hasta mañana",
-            "Quién se va?"
-        ],
-        author: "Estefanía Lopez."
-    },
-    
-    {
-        quote: [
-            "El Docker no se me levanta"
-        ],
-        author: "Jesús Moreno."
+        author: "Pepón García"
     }
 ];
